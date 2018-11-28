f9a75cacb2eef0277b08ab8bd1aa5a859d3ab487
MoA <honomoa@gmail.com>
2018-11-28 10:34:04 +0800

    Fixes image name

--- build_image.sh
+++ build_image.sh
@@ -4,5 +4,5 @@ ANSIBLE_VERSION=2.7.2
 
 docker build \
   --build-arg ANSIBLE_VERSION=${ANSIBLE_VERSION} \
-  -t honomoa/docker-jenkins-ansible:${ANSIBLE_VERSION}-alpine \
+  -t honomoa/jenkins-ansible:${ANSIBLE_VERSION}-alpine \
   .
